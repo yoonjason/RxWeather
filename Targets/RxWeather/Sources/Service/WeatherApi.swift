de22567aae20b26f04ad876c1d97f7a663cf2f4c
yoon <yeong806@gmail.com>
2022-03-02 17:45:34 +0900

    fix:  주석 처리

--- Targets/RxWeather/Sources/Service/WeatherApi.swift
+++ Targets/RxWeather/Sources/Service/WeatherApi.swift
@@ -13,38 +13,59 @@ import CoreLocation
 import NSObject_Rx
 
 class WeatherApi: NSObject, WeatherApiType {
-
+    /**
+    현재 날씨 방출, 주로 UI 바인딩에 활용
+     */
     private let summaryRelay = BehaviorRelay<WeatherDataType?>(value: nil)
+
+    /**
+     예보 목록 방출, 주로 UI 바인딩에 활용
+     */
     private let forecastRelay = BehaviorRelay<[WeatherDataType]>(value: [])
+
+    /**
+     URL session
+     */
     private let urlSession = URLSession.shared
 
+    /**
+     location을 파라미터로 받아서, 현재 날씨를 옵져버블로 방출
+     */
     private func fetchSummary(location: CLLocation) -> Observable<WeatherDataType?> {
         let request = composeUrlRequest(endpoint: summaryEndpoint, from: location)
-
+        /**
+         RxCocoa가 제공하는 데이터 메서드, 요청을 전달한 다음 결과를 데이터 형식으로 방출하는 옵져버블 리턴
+         */
         return urlSession.rx.data(request: request)
-            .map { data in
+            .map {
             let decoder = JSONDecoder()
-            return try decoder.decode(WeatherSummary.self, from: data)
+            return try decoder.decode(WeatherSummary.self, from: $0)
         }
-            .map { data in
-            WeatherData(summary: data)
+            .map { //WeatherData type으로 변환
+            WeatherData(summary: $0)
         }
             .catchAndReturn(nil)
     }
-
+    /**
+     예보 데이터
+     */
     private func fetchForecast(location: CLLocation) -> Observable<[WeatherDataType]> {
         let request = composeUrlRequest(endpoint: forecastEndpoint, from: location)
         return urlSession.rx.data(request: request)
             .map {
+            /**
+                 Forecast 타입으로 파싱 후,
+                 */
             let decoder = JSONDecoder()
             let forecast = try decoder.decode(Forecast.self, from: $0)
 
             return forecast.list.map(WeatherData.init)
         }
             .catchAndReturn([])
-
     }
 
+
+    @discardableResult
     func fetch(location: CLLocation) -> Observable<(WeatherDataType?, [WeatherDataType])> {
         let summary = fetchSummary(location: location)
         let forecast = fetchForecast(location: location)
@@ -55,6 +76,10 @@ class WeatherApi: NSObject, WeatherApiType {
                 self?.forecastRelay.accept(result.1)
             })
             .disposed(by: rx.disposeBag)
+            /**
+             두 시퀀스가 이벤트를 발생시켜야만 합쳐진 시퀀스에서 이벤트가 발생
+             */
         return Observable.combineLatest(summaryRelay.asObservable(), forecastRelay.asObservable())
     }
+
 }
